12680dd969d6853b86be9ca79b874ff46da0a29e
Imran Z <ancientlahore@gmail.com>
2021-08-12 06:23:56 +0500

    Update api.php

--- api.php
+++ api.php
@@ -1,1 +1,19 @@
+<?php
 
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json;charset=UTF-8");
+header('x-hello-world: Imran Z');
+
+$name = isset($_GET['name'])?addslashes($_GET['name']):'';
+
+$result = array();
+$result['ip'] = $_SERVER['REMOTE_ADDR'];
+
+
+if($name)
+$result['greeting'] = $name;
+
+
+echo json_encode($result);
+?>
